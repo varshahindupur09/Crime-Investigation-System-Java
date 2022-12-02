57ae6ef254745f2d653c98833aa905e1240ea28b
Saloni <talwar.sa@northeastern.edu>
2022-12-02 12:52:12 -0500

    Crime Branch Enterprise Structure

---
+++ src/crime_branch_enterprise/model/Victim.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Victim {
+    private String victimID;
+    private String name;
+    private String fatherName;
+    private long phoneNo;
+    private String remarks;
+
+    
+    public String getVictimID() {
+        return victimID;
+    }
+
+    public void setVictimID(String victimID) {
+        this.victimID = victimID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+    
+    
+    
+}
