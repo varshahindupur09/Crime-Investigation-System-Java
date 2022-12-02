9d4e2a3ea404c25e47e139b9be6130f9a6a8dccc
Saloni <talwar.sa@northeastern.edu>
2022-12-02 13:20:59 -0500

    Crime Enterprise- Complainer.java initial commit

---
+++ src/crime_branch_enterprise/model/Complainer.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Complainer {
+    
+    public String complainerID;
+    public String complainerName;
+    public String complainerFatherName;
+    public String complainerAddress;
+    public int complainerAge;
+    public String complainerGender;
+    public long complainerPhoneNumber;
+    public String complainerCNIC;
+
+    public String getComplainerID() {
+        return complainerID;
+    }
+
+    public void setComplainerID(String complainerID) {
+        this.complainerID = complainerID;
+    }
+
+    public String getComplainerName() {
+        return complainerName;
+    }
+
+    public void setComplainerName(String complainerName) {
+        this.complainerName = complainerName;
+    }
+
+    public String getComplainerFatherName() {
+        return complainerFatherName;
+    }
+
+    public void setComplainerFatherName(String complainerFatherName) {
+        this.complainerFatherName = complainerFatherName;
+    }
+
+    public String getComplainerAddress() {
+        return complainerAddress;
+    }
+
+    public void setComplainerAddress(String complainerAddress) {
+        this.complainerAddress = complainerAddress;
+    }
+
+    public int getComplainerAge() {
+        return complainerAge;
+    }
+
+    public void setComplainerAge(int complainerAge) {
+        this.complainerAge = complainerAge;
+    }
+
+    public String getComplainerGender() {
+        return complainerGender;
+    }
+
+    public void setComplainerGender(String complainerGender) {
+        this.complainerGender = complainerGender;
+    }
+
+    public long getComplainerPhoneNumber() {
+        return complainerPhoneNumber;
+    }
+
+    public void setComplainerPhoneNumber(long complainerPhoneNumber) {
+        this.complainerPhoneNumber = complainerPhoneNumber;
+    }
+
+    public String getComplainerCNIC() {
+        return complainerCNIC;
+    }
+
+    public void setComplainerCNIC(String complainerCNIC) {
+        this.complainerCNIC = complainerCNIC;
+    }
+    
+    
+    
+}
