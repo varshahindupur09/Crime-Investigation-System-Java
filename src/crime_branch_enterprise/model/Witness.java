804a0a6a8877c25aec2664a13a3497fb2642713a
Saloni <talwar.sa@northeastern.edu>
2022-12-02 13:06:37 -0500

    Crime Enterprise- Witness.java initial commit

---
+++ src/crime_branch_enterprise/model/Witness.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Witness {
+    
+    private String witnessID;
+    private String witnessName;
+    private String witnessFatherName;
+    private long witnessContactNo;
+    private String witnessRemark;
+
+    public String getWitnessID() {
+        return witnessID;
+    }
+
+    public void setWitnessID(String witnessID) {
+        this.witnessID = witnessID;
+    }
+
+    public String getWitnessName() {
+        return witnessName;
+    }
+
+    public void setWitnessName(String witnessName) {
+        this.witnessName = witnessName;
+    }
+
+    public String getWitnessFatherName() {
+        return witnessFatherName;
+    }
+
+    public void setWitnessFatherName(String witnessFatherName) {
+        this.witnessFatherName = witnessFatherName;
+    }
+
+    public long getWitnessContactNo() {
+        return witnessContactNo;
+    }
+
+    public void setWitnessContactNo(long witnessContactNo) {
+        this.witnessContactNo = witnessContactNo;
+    }
+
+    public String getWitnessRemark() {
+        return witnessRemark;
+    }
+
+    public void setWitnessRemark(String witnessRemark) {
+        this.witnessRemark = witnessRemark;
+    }
+    
+    
+    
+}
