e6a5217dcc242d488a8e19ba06b229bc148e5c6d
Saloni <talwar.sa@northeastern.edu>
2022-12-02 13:12:52 -0500

    Crime Enterprise- PoliceStation.java initial commit

---
+++ src/crime_branch_enterprise/model/PoliceStation.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class PoliceStation {
+    
+    private String policeStationID;
+    private String policeStationName;
+    private String policeStationLocation;
+    private String policeStationCity;
+    private String policeStationPhoneNumber;
+    // Get officerID from OfficerRecord.java
+    private String policeStationRemarks;
+    
+}
