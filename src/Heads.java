a221f3ee48072915889da57931698392ee2c1a5e
Saloni <talwar.sa@northeastern.edu>
2022-12-02 13:09:51 -0500

    Crime Enterprise- Heads.java initial commit

---
+++ src/Heads.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Heads {
+    
+    private String headID;
+    private String headName;
+    private String headDescription;
+
+    public String getHeadID() {
+        return headID;
+    }
+
+    public void setHeadID(String headID) {
+        this.headID = headID;
+    }
+
+    public String getHeadName() {
+        return headName;
+    }
+
+    public void setHeadName(String headName) {
+        this.headName = headName;
+    }
+
+    public String getHeadDescription() {
+        return headDescription;
+    }
+
+    public void setHeadDescription(String headDescription) {
+        this.headDescription = headDescription;
+    }
+    
+    
+    
+}
