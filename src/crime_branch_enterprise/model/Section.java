3ec99efa464fbfc3d664fd85d9766a418f8be1db
hindupurv <hindupur.v@northeastern.edu>
2022-12-02 20:14:39 -0500

    changes taken
    taken changes made by saloni

---
+++ src/crime_branch_enterprise/model/Section.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Section {
+    
+    public String sectionID;
+    public String description;
+    public String remarks;
+
+    public String getSectionID() {
+        return sectionID;
+    }
+
+    public void setSectionID(String sectionID) {
+        this.sectionID = sectionID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+    
+    
+    
+}
---
+++ src/crime_branch_enterprise/model/VictimFir.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crime_branch_enterprise.model;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class VictimFir {
+    // get victimID from Victim.java
+    // get crimeID from CrimeRegister.java
+}
