a81a012cb8195f635cb4b9207707ee205b428b3d
Saloni <talwar.sa@northeastern.edu>
2022-12-08 22:51:12 -0500

    News Enterprise- Newspaper.java initial commit

---
+++ src/NewsModel/Newspaper.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package NewsModel;
+
+/**
+ *
+ * @author Sal <your.name at your.org>
+ */
+public class Newspaper {
+    
+    private String npId;
+    private String npPrice;
+    private int npNoOfPages;
+    private String npDate;
+    private String npPublicationId;
+
+    public String getNpId() {
+        return npId;
+    }
+
+    public void setNpId(String npId) {
+        this.npId = npId;
+    }
+
+    public String getNpPrice() {
+        return npPrice;
+    }
+
+    public void setNpPrice(String npPrice) {
+        this.npPrice = npPrice;
+    }
+
+    public int getNpNoOfPages() {
+        return npNoOfPages;
+    }
+
+    public void setNpNoOfPages(int npNoOfPages) {
+        this.npNoOfPages = npNoOfPages;
+    }
+
+    public String getNpDate() {
+        return npDate;
+    }
+
+    public void setNpDate(String npDate) {
+        this.npDate = npDate;
+    }
+
+    public String getNpPublicationId() {
+        return npPublicationId;
+    }
+
+    public void setNpPublicationId(String npPublicationId) {
+        this.npPublicationId = npPublicationId;
+    }
+    
+    
+    
+}
