3017eef39767df834ceb2ce5acf56fdb5c36757d
hindupurv <hindupur.v@northeastern.edu>
2022-12-11 01:36:06 -0500

    database adding for Crime Branch
    Values added

--- src/crime_branch_enterprise/model/CrimeRegister.java
+++ src/crime_branch_enterprise/model/CrimeRegister.java
@@ -10,35 +10,35 @@ package crime_branch_enterprise.model;
  */
 public class CrimeRegister {
     
-    private String crimeID;
+//    private String crimeID;
     // Get headID from Heads.java
     // Get headDescription from Heads.java
     private String dateOfOffence; //filled by customer
     
-    private long firNumber;
-    private String dateOfReport; //must be connected to FIR Number
-    private String underSection;
+//    private long firNumber;
+//    private String dateOfReport; //must be connected to FIR Number
+//    private String underSection;
     
-    private String caseStatus; //for new will be added by us - in progress
-    private boolean arrested;
-    private String challan;
+//    private String caseStatus; //for new will be added by us - in progress
+//    private boolean arrested;
+//    private String challan;
     
     //Officer ID from OfficerRecord.java
-    private String timeOfCrime;
-    private String description;
+//    private String timeOfCrime;
+//    private String description;
     
     // Get accusedID from Accused.java
     // Get complainerID from Complainer.java
     // Get policeStationID from PoliceStation.java
     //Get accusedName, accusedFatherName, accusedAddress CNIC, age , phone and gender from Accused.java
 
-    public String getCrimeID() {
-        return crimeID;
-    }
-
-    public void setCrimeID(String crimeID) {
-        this.crimeID = crimeID;
-    }
+//    public String getCrimeID() {
+//        return crimeID;
+//    }
+//
+//    public void setCrimeID(String crimeID) {
+//        this.crimeID = crimeID;
+//    }
 
     public String getDateOfOffence() {
         return dateOfOffence;
@@ -48,69 +48,69 @@ public class CrimeRegister {
         this.dateOfOffence = dateOfOffence;
     }
 
-    public long getFirNumber() {
-        return firNumber;
-    }
+//    public long getFirNumber() {
+//        return firNumber;
+//    }
+//
+//    public void setFirNumber(long firNumber) {
+//        this.firNumber = firNumber;
+//    }
+//
+//    public String getUnderSection() {
+//        return underSection;
+//    }
+//
+//    public void setUnderSection(String underSection) {
+//        this.underSection = underSection;
+//    }
 
-    public void setFirNumber(long firNumber) {
-        this.firNumber = firNumber;
-    }
+//    public String getDateOfReport() {
+//        return dateOfReport;
+//    }
+//
+//    public void setDateOfReport(String dateOfReport) {
+//        this.dateOfReport = dateOfReport;
+//    }
 
-    public String getUnderSection() {
-        return underSection;
-    }
+//    public String getCaseStatus() {
+//        return caseStatus;
+//    }
+//
+//    public void setCaseStatus(String caseStatus) {
+//        this.caseStatus = caseStatus;
+//    }
+//
+//    public boolean isArrested() {
+//        return arrested;
+//    }
+//
+//    public void setArrested(boolean arrested) {
+//        this.arrested = arrested;
+//    }
+//
+//    public String getChallan() {
+//        return challan;
+//    }
+//
+//    public void setChallan(String challan) {
+//        this.challan = challan;
+//    }
 
-    public void setUnderSection(String underSection) {
-        this.underSection = underSection;
-    }
-
-    public String getDateOfReport() {
-        return dateOfReport;
-    }
-
-    public void setDateOfReport(String dateOfReport) {
-        this.dateOfReport = dateOfReport;
-    }
-
-    public String getCaseStatus() {
-        return caseStatus;
-    }
-
-    public void setCaseStatus(String caseStatus) {
-        this.caseStatus = caseStatus;
-    }
-
-    public boolean isArrested() {
-        return arrested;
-    }
-
-    public void setArrested(boolean arrested) {
-        this.arrested = arrested;
-    }
-
-    public String getChallan() {
-        return challan;
-    }
-
-    public void setChallan(String challan) {
-        this.challan = challan;
-    }
-
-    public String getTimeOfCrime() {
-        return timeOfCrime;
-    }
-
-    public void setTimeOfCrime(String timeOfCrime) {
-        this.timeOfCrime = timeOfCrime;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
+//    public String getTimeOfCrime() {
+//        return timeOfCrime;
+//    }
+//
+//    public void setTimeOfCrime(String timeOfCrime) {
+//        this.timeOfCrime = timeOfCrime;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//    
     
 }
